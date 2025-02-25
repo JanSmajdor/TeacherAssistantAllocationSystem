f500020c69278075c7393e86c40a83cae6de03d6
Jan Smajdor <j-agaa@hotmail.com>
2025-02-25 13:43:16 +0000

    removed duplicate home route and added route for edit_account

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('auth/login');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/edit_account', [App\Http\Controllers\UserController::class, 'show'])->name('edit_account');
