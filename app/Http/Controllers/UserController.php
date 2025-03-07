d2574b84840bf811b8357be6b3625ad2991bc7f6
Jan Smajdor <j-agaa@hotmail.com>
2025-03-07 09:22:12 +0000

    retrieve teaching assistant details and availability for account editing view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\AreaOfKnowledge;
+use App\Models\TAAreaOfKnowledge;
+use App\Models\Availability;
 
 class UserController extends Controller 
 {
@@ -26,7 +29,23 @@ class UserController extends Controller
     public function show()
     {
         $user = Auth::user();
+        $ta_details = User::select(
+            'users.first_name',
+            'users.last_name',
+            'users.email',
+            'users.password',
+            'teaching_assistants.contracted_hours'
+        )
+        ->join('teaching_assistants', 'teaching_assistants.user_id', '=', 'users.id')
+        ->where('users.id', $user->id)
+        ->first();
 
-        return view('edit_account', compact('user'));
+        $availability = Availability::where('ta_id', $user->id)->get();
+
+        $all_areas = AreaOfKnowledge::all();
+
+        $ta_areas_of_knowledge = TAAreaOfKnowledge::where('ta_id', $user->id)->pluck('area_id')->toArray();
+
+        return view('edit_account', compact('ta_details', 'availability', 'all_areas', 'ta_areas_of_knowledge'));
     }
 }
